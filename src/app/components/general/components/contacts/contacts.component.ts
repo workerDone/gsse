01ec68f62960888395bbef825ec6707235cb293e
root <chmutkola@gmail.com>
2019-01-13 14:22:37 +0200

    second

--- src/app/components/general/components/contacts/contacts.component.ts
+++ src/app/components/general/components/contacts/contacts.component.ts
@@ -22,10 +22,10 @@ export class ContactsComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9 ]{1,}')]],
+      name: ['', [Validators.required, Validators.pattern('[a-zа-яА-ЯA-Z0-9 ]{1,296}')]],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required]],
-      message: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9 ]{1,296}'), Validators.maxLength(256)]]
+      message: ['', [Validators.required, Validators.pattern('[a-zа-яА-ЯA-Z0-9 ]{1,296}'), Validators.maxLength(256)]]
     });
   }
   onSubmit() {
@@ -34,17 +34,19 @@ export class ContactsComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
-    const formData = new FormData();
+    // const formData = new FormData();
 
-    formData.set('entry.507307065', this.form.value.name );
-    formData.set('entry.1617704409', this.form.value.email );
-    formData.set('entry.2113232328', this.form.value.phone );
-    formData.set('entry.1707151155', this.form.value.message );
+    // formData.set('entry.507307065', this.form.value.name );
+    // formData.set('entry.1617704409', this.form.value.email );
+    // formData.set('entry.2113232328', this.form.value.phone );
+    // formData.set('entry.1707151155', this.form.value.message );
 
-    const path = `/forms/d/e/1FAIpQLSdcd3MhjlJjO6DEi6jPvty_FhGQgNq-UEgvuSV2w1vNMx6AKA/formResponse`;
-    this.addGoogle(path, formData).toPromise()
+    // const path = `/forms/d/e/1FAIpQLSdcd3MhjlJjO6DEi6jPvty_FhGQgNq-UEgvuSV2w1vNMx6AKA/formResponse`;
+    
+    const body = Object.assign({id: 250011}, this.form.value);
+    this.sendEmail(body).toPromise()
     .then((data) => console.log(data))
-    .catch((err) => console.log(err, 'sssserr'));
+    .catch((err) => console.log(err, 'catch'));
     if (this.form.valid) {
       this.successHandler();
     }
@@ -59,10 +61,10 @@ export class ContactsComponent implements OnInit {
     }, 3000);
   }
 
-  addGoogle(path: string, body: Object = {}): Observable<any> {
-    const baseApiUrl = 'https://docs.google.com';
+  sendEmail( body: Object = {}): Observable<any> {
+    const baseApiUrl = 'https://emailinteragrokiev.herokuapp.com/';
     return this.http
-      .post(`${baseApiUrl}${path}`, body)
+      .post(`${baseApiUrl}`, body)
       .pipe(
         catchError((err) => this.formatErrors(err))
       );
